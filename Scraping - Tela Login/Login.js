ec4f06935576c45aa4f72bcfc0e444316643a03c
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-09-26 11:20:17 -0300

    Evaluate

--- Scraping - Tela Login/Login.js
+++ Scraping - Tela Login/Login.js
@@ -17,5 +17,9 @@ const URL = `https://unsplash.com/`;
   await page.waitForNavigation();
   //Depois de fazer o login ou carregar qualquer página deve esperar o carregamento completo dos cookies e da pagina, para depois continuar a executar as ações, poir isso deve se utilizar o waitForNavegation
 
+  await page.waitForSelector('.sucess', {visible:true}); //procurar algum eleemento ou css que só aparece após a pagina ser carregado.Exemplo: esperar até que a classe .sucess se torne visivel para realizar as demais ações
+
+  // await page.evaluate(() => {}) //Espera uma função que será executada no browser(usando o DOM) //https://www.youtube.com/watch?v=K5yYBJhix5A&ab_channel=Rocketseat
+
    // await browser.close(); // fechando o browser
 })(); // executar a função assim que iniciar
